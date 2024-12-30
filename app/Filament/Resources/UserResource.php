a9cabadc63b6a1c3cc1e127a686300cd6337530a
Nolan Shrestha <96360893+NolanShrestha@users.noreply.github.com>
2024-12-30 11:33:17 +0545

    Table Repeater

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Awcodes\TableRepeater\Components\TableRepeater;
+use Awcodes\TableRepeater\Header;
+use Filament\Support\Enums\Alignment;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+    
+                TableRepeater::make('users')
+                    ->showLabels()
+                    ->emptyLabel('There are no registered users!')
+                    ->headers([
+                        Header::make('name')
+                            ->markAsRequired()
+                            ->align(Alignment::Center)
+                            ->width('150px'),
+                        Header::make('email')
+                    ])
+                    ->renderHeader(false)
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->label('Name')
+                            ->required(),
+                        Forms\Components\TextInput::make('email')
+                            ->label('Email')
+                            ->email()
+                            ->required(),
+                    ])
+                    ->columnSpan('full')
+          
+                    ]);
+    }
+
+    public static function table(Tables\Table $table): Tables\Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->searchable(),
+                Tables\Columns\TextColumn::make('email')->searchable(),
+            ])
+            ->filters([
+                
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            // Define any relations if needed
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
